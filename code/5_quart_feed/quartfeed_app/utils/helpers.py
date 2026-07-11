d967d201b515f6b4bab9e8248047d03642e76f8b
Jorge Escobar <j@jorge3.com>
2026-07-10 23:57:46 -0400

    5.5.1: login_required decorator (utils/helpers.py)

--- code/5_quart_feed/quartfeed_app/utils/helpers.py
+++ code/5_quart_feed/quartfeed_app/utils/helpers.py
@@ -1,5 +1,7 @@
-from typing import Any, Optional
+from functools import wraps
+from typing import Any, Callable, Optional
 
+from quart import redirect, request, session, url_for
 from sqlalchemy import select
 from sqlalchemy.engine import Row
 
@@ -11,3 +13,13 @@ async def get_user_by_username(conn: Any, username: str) -> Optional[Row]:
         select(user_table).where(user_table.c.username == username)
     )
     return result.fetchone()
+
+
+def login_required(f: Callable) -> Callable:
+    @wraps(f)
+    async def decorated_function(*args: Any, **kwargs: Any) -> Any:
+        if session.get("username") is None:
+            return redirect(url_for("user_app.login", next=request.url))
+        return await f(*args, **kwargs)
+
+    return decorated_function

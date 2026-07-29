0a4bc8309f03e049de70990b6ca1fa513b158d1b
Jorge Escobar <j@jorge3.com>
2026-07-29 17:52:17 -0400

    5.8.7: register the post models with alembic (migrations/env.py)

--- code/5_quart_feed/quartfeed_app/migrations/env.py
+++ code/5_quart_feed/quartfeed_app/migrations/env.py
@@ -15,6 +15,7 @@ sys.path.insert(0, os.getcwd())
 from db import metadata
 from user.models import user_table  # noqa: F401
 from relationship.models import relationship_table  # noqa: F401
+from post.models import post_table, post_image_table  # noqa: F401
 config.set_main_option(
     "sqlalchemy.url",
     f"postgresql+asyncpg://{os.environ['DB_USERNAME']}:{os.environ['DB_PASSWORD']}"
